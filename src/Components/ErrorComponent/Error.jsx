68d78be8f61390b4f46a56244a6124132eeb0cf0
LeysanValitova <leysan.2520@bk.ru>
2024-05-27 19:27:18 +0300

    fixes

--- src/Components/ErrorComponent/Error.jsx
+++ src/Components/ErrorComponent/Error.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
+import Modal from '@mui/material/Modal';
+import Box from '@mui/material/Box';
 
-const ErrorComponent = ({ error, сlearError }) => {
+const ErrorComponent = ({ error, clearError }) => {
     return (
-        <div>
-            <h3>Error: {error}</h3>
-            <button onClick={сlearError}>Close</button>
-        </div>
+        <Modal open={error !== ""} onClose={clearError}>
+            <Box sx={{ position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', bgcolor: 'background.paper', border: '2px solid #000', boxShadow: 24, p: 4 }}>
+                <h3>Error: {error}</h3>
+                <button onClick={clearError}>Close</button>
+            </Box>
+        </Modal>
     );
 }
-
 export default ErrorComponent;
