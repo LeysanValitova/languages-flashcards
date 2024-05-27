0df584896021bd30eb386d8f5fea458381241f18
LeysanValitova <leysan.2520@bk.ru>
2024-05-27 19:33:12 +0300

    fixes

--- src/Components/Table/Table.jsx
+++ src/Components/Table/Table.jsx
@@ -1,35 +1,53 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useRef } from 'react'
 import styles from './Table.module.css'
 import TableRow from '../TableRow/TableRow'
 import { inject, observer } from 'mobx-react'
+import ErrorComponent from '../ErrorComponent/Error';
 
 
-
- function Table({wordsStore}) {
-  const {words, isLoaded, loadData} = wordsStore;
+function Table({ wordsStore }) {
+  const { words, loadData, error, setError, clearError } = wordsStore;
+  const isLoaded = useRef(false);
 
   useEffect(() => {
-    loadData();
-  }, [loadData]);
+    const fetchData = async () => {
+      try {
+        await loadData();
+        isLoaded.current = true;
+      } catch (error) {
+        setError('Failed to load data from the server');
+        isLoaded.current = true;
+      }
+    };
 
-   if (!isLoaded) { return <div>Loading...</div>;}
+    fetchData();
+  }, [loadData, setError]);
+
+  if (!isLoaded.current) {
+    return <div>Loading...</div>;
+  }
+
   return (
-    <table className={styles.table_container}>
+    <div>
+      {error && <ErrorComponent error={error} clearError={clearError} />}
+      <table className={styles.table_container}>
         <thead className={styles.table_nav}>
-            <tr>
-                <th>№</th>
-                <th>English</th>
-                <th>Transcription</th>
-                <th>Translation</th>
-                <th>Actions</th>
-            </tr>
+          <tr>
+            <th>№</th>
+            <th>English</th>
+            <th>Transcription</th>
+            <th>Translation</th>
+            <th>Actions</th>
+          </tr>
         </thead>
         <tbody>
-            {words.map((word)=> (
-            <TableRow rowData={word} key={word.id} />))}
+          {words.map((word) => (
+            <TableRow rowData={word} key={word.id} />
+          ))}
         </tbody>
-    </table>
-  )
+      </table>
+    </div>
+  );
 }
 
 export default inject('wordsStore')(observer(Table));
